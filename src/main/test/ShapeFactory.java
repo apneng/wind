ca9363f2effafd442cd9d97d0a951e82a36fda6b
apneng <903060@163.com>
2018-04-09 17:06:32 +0800

    f

---
+++ src/main/test/ShapeFactory.java
@@ -0,0 +1,19 @@
+public class ShapeFactory extends AbstractFactory {
+    @Override
+    Color getColor(String color) {
+        return null;
+    }
+
+    @Override
+    Shape getShape(String shape) {
+       if(shape == null){
+           return null;
+       }
+       if(shape.equalsIgnoreCase("rectangle")){
+           return new Rectangle();
+       }else if(shape.equalsIgnoreCase("square")){
+           return new Square();
+       }
+       return null;
+    }
+}
